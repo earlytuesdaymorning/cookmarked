7e33db1c49a22bd9a04bc2f0548c066e182b1268
Mariah (Tuesday) Hernandez <rahrah93@gmail.com>
2022-07-12 12:41:09 -0700

    adds recipe model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,19 @@
+from dis import Instruction
+from email.mime import image
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Recipe(models.Model):
+    # def __init__(self, label, image, mealtype, ingredients, instructions):
+    label = models.CharField(max_length=150)
+    image = models.CharField(max_length=500)
+    mealtype = models.CharField(max_length=100)
+    ingredients = models.TextField(max_length=900)
+    instructions = models.TextField(max_length=1500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.label
+
