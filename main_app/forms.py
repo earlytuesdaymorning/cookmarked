4b44af93bbe7ad30fdcda402daabb90e874b55c1
Mariah (Tuesday) Hernandez <rahrah93@gmail.com>
2022-07-13 14:13:51 -0700

    save before break

---
+++ main_app/forms.py
@@ -0,0 +1,8 @@
+
+from django.forms import ModelForm
+from .models import Instruction
+
+class InstructionForm(ModelForm):
+  class Meta:
+    model = Instruction
+    fields = ['step', 'text']
--- main_app/models.py
+++ main_app/models.py
@@ -8,9 +8,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Recipe(models.Model):
     # def __init__(self, label, image, mealtype, ingredients, instructions):
-    label = models.CharField(max_length=150)
+    label = models.CharField("Recipe Title", max_length=150)
     # image = models.CharField(max_length=500)
-    mealtype = models.CharField(max_length=100)
+    mealtype = models.CharField("Meal Type", max_length=100)
     ingredients = models.TextField(max_length=900)
     # instructions = models.TextField(max_length=1500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -27,6 +27,8 @@ class Instruction(models.Model):
 
     def __str__(self):
         return {self.text}
+
+    
 
 class Photo(models.Model):
     url = models.CharField(max_length=200)
